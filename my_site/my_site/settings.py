956b557ad7f71b681dc7062479769d4964819d37
konrad <ostrowskikonrad71@gmail.com>
2023-02-18 17:53:47 +0100

    update setting apps

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'my_app.apps.MyAppConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
